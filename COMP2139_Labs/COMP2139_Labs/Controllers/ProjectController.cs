cec6d6f8521d4d94ccd83276dcbfd83d7c222f47
Nisarg0904 <nisarg.bhatti0904@gmail.com>
2024-01-26 00:57:59 -0500

    Everything till lab 2 , in class exercise 1

---
+++ COMP2139_Labs/COMP2139_Labs/Controllers/ProjectController.cs
@@ -0,0 +1,39 @@
+﻿using COMP2139_Labs.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP2139_Labs.Controllers
+{
+    public class ProjectController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var project = new List<Project>()
+            {
+                new Project { ProjectId = 1,Name="Project 1", Description="My First Project" }
+
+            };
+            return View(project);
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(Project project)
+        {
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var project=new Project { ProjectId = id, Name="Project name" + id,Description="Project describtion "+id };
+            return View(project);
+        }
+
+    }
+}
