b7cc9b7ef8cd6206617c04378e4221e497e4383a
solaresDC <daniel.chavez200326@gmail.com>
2024-02-26 00:40:32 -0500

    aad

--- Assign/Assignment/Assignment/obj/Debug/net8.0/Assignment.AssemblyInfo.cs
+++ Assign/Assignment/Assignment/obj/Debug/net8.0/Assignment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c4ec435335023afdc92c48524df5a28eeace91f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7656ef256b85048ae1e61d7e6148cd5ae73f693a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
