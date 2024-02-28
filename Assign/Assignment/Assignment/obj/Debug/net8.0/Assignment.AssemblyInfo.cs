780290a980f37e8acb1a44298f19c31a47451efb
Nisarg0904 <nisarg.bhatti0904@gmail.com>
2024-02-27 21:17:54 -0500

    Final Push

--- Assign/Assignment/Assignment/obj/Debug/net8.0/Assignment.AssemblyInfo.cs
+++ Assign/Assignment/Assignment/obj/Debug/net8.0/Assignment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05965a0502cfe19224d4d30b0ac71e58c0033fbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fca769e0f502b73c621bdc32dd556a510b4440d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
