3d2fe4cf99aa2020d1c345b4a990023d7e884ecc
Nisarg0904 <nisarg.bhatti0904@gmail.com>
2024-04-12 08:14:27 -0400

    a small error in adding the role ! fixed!

--- COMP2139_Labs/COMP2139_Labs/obj/Debug/net8.0/COMP2139_Labs.AssemblyInfo.cs
+++ COMP2139_Labs/COMP2139_Labs/obj/Debug/net8.0/COMP2139_Labs.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("COMP2139_Labs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a251758a4bb3decbfa69ccbb4724d4ce9a494496")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c62ffde12248c5d6c79604b3ef24eae78fd467a")]
 [assembly: System.Reflection.AssemblyProductAttribute("COMP2139_Labs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("COMP2139_Labs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
