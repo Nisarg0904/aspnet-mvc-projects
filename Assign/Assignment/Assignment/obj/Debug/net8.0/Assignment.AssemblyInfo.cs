635a68019206f278dc0cf89fde8b0ec7ca13e8cd
Nisarg0904 <nisarg.bhatti0904@gmail.com>
2024-04-16 20:20:52 -0400

    bla

--- Assign/Assignment/Assignment/obj/Debug/net8.0/Assignment.AssemblyInfo.cs
+++ Assign/Assignment/Assignment/obj/Debug/net8.0/Assignment.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+693a7ea49fe552fa7b7268f58c41b20bf96957df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29ba4feb1ba960d7ba416275905ead7b9a5bb36a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
